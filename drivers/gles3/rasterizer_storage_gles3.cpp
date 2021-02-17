d9d5412efc3661054ecfa3997ce9ecc5245199a6
Saracen <SaracenOne@gmail.com>
2021-02-17 22:35:35 +0000

    Add size check when getting cached textures

--- drivers/gles3/rasterizer_storage_gles3.cpp
+++ drivers/gles3/rasterizer_storage_gles3.cpp
@@ -1110,7 +1110,7 @@ Ref<Image> RasterizerStorageGLES3::texture_get_data(RID p_texture, int p_layer) 
 	ERR_FAIL_COND_V(!texture->active, Ref<Image>());
 	ERR_FAIL_COND_V(texture->data_size == 0 && !texture->render_target, Ref<Image>());
 
-	if (p_layer >= 0 && !texture->images[p_layer].is_null()) {
+	if (p_layer >= 0 && p_layer < texture->images.size() && !texture->images[p_layer].is_null()) {
 		return texture->images[p_layer];
 	}
 
