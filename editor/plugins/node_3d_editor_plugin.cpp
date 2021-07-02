4aa66e58d9bfbc09be103df901704b89ec254ec5
reduz <reduzio@gmail.com>
2021-07-02 20:14:19 -0300

    Fix Render Info
    * Fixed and redone the process to obtain render information from a viewport
    * Some stats, such as material changes are too difficult to guess on Vulkan, were removed.
    * Separated visible and shadow stats, which causes confusion.
    * Texture, buffer and general video memory can be queried now.
    * Fixed the performance metrics too.

--- editor/plugins/node_3d_editor_plugin.cpp
+++ editor/plugins/node_3d_editor_plugin.cpp
@@ -2516,13 +2516,11 @@ void Node3DEditorViewport::_notification(int p_what) {
 							": %dx%d (%.1fMP)\n",
 							viewport->get_size().x,
 							viewport->get_size().y,
-							viewport->get_size().x * viewport->get_size().y * 0.000'001);
-			text += TTR("Objects Drawn") + ": " + itos(viewport->get_render_info(Viewport::RENDER_INFO_OBJECTS_IN_FRAME)) + "\n";
-			text += TTR("Material Changes") + ": " + itos(viewport->get_render_info(Viewport::RENDER_INFO_MATERIAL_CHANGES_IN_FRAME)) + "\n";
-			text += TTR("Shader Changes") + ": " + itos(viewport->get_render_info(Viewport::RENDER_INFO_SHADER_CHANGES_IN_FRAME)) + "\n";
-			text += TTR("Surface Changes") + ": " + itos(viewport->get_render_info(Viewport::RENDER_INFO_SURFACE_CHANGES_IN_FRAME)) + "\n";
-			text += TTR("Draw Calls") + ": " + itos(viewport->get_render_info(Viewport::RENDER_INFO_DRAW_CALLS_IN_FRAME)) + "\n";
-			text += TTR("Vertices") + ": " + itos(viewport->get_render_info(Viewport::RENDER_INFO_VERTICES_IN_FRAME));
+							viewport->get_size().x * viewport->get_size().y * 0.000001);
+
+			text += vformat(TTR("\nObjects Drawn: %d   (%d in shadow)\n"), viewport->get_render_info(Viewport::RENDER_INFO_TYPE_VISIBLE, Viewport::RENDER_INFO_OBJECTS_IN_FRAME), viewport->get_render_info(Viewport::RENDER_INFO_TYPE_SHADOW, Viewport::RENDER_INFO_OBJECTS_IN_FRAME));
+			text += vformat(TTR("Vertices Drawn: %d   (%d in shadow)\n"), viewport->get_render_info(Viewport::RENDER_INFO_TYPE_VISIBLE, Viewport::RENDER_INFO_VERTICES_IN_FRAME), viewport->get_render_info(Viewport::RENDER_INFO_TYPE_SHADOW, Viewport::RENDER_INFO_VERTICES_IN_FRAME));
+			text += vformat(TTR("Draw Calls: %d   (%d in shadow)"), viewport->get_render_info(Viewport::RENDER_INFO_TYPE_VISIBLE, Viewport::RENDER_INFO_DRAW_CALLS_IN_FRAME), viewport->get_render_info(Viewport::RENDER_INFO_TYPE_SHADOW, Viewport::RENDER_INFO_DRAW_CALLS_IN_FRAME));
 
 			info_label->set_text(text);
 		}
--- servers/rendering/renderer_rd/forward_mobile/render_forward_mobile.cpp
+++ servers/rendering/renderer_rd/forward_mobile/render_forward_mobile.cpp
@@ -555,7 +555,7 @@ void RenderForwardMobile::_render_shadow_begin() {
 	render_list[RENDER_LIST_SECONDARY].clear();
 }
 
-void RenderForwardMobile::_render_shadow_append(RID p_framebuffer, const PagedArray<GeometryInstance *> &p_instances, const CameraMatrix &p_projection, const Transform3D &p_transform, float p_zfar, float p_bias, float p_normal_bias, bool p_use_dp, bool p_use_dp_flip, bool p_use_pancake, const Plane &p_camera_plane, float p_lod_distance_multiplier, float p_screen_lod_threshold, const Rect2i &p_rect, bool p_flip_y, bool p_clear_region, bool p_begin, bool p_end) {
+void RenderForwardMobile::_render_shadow_append(RID p_framebuffer, const PagedArray<GeometryInstance *> &p_instances, const CameraMatrix &p_projection, const Transform3D &p_transform, float p_zfar, float p_bias, float p_normal_bias, bool p_use_dp, bool p_use_dp_flip, bool p_use_pancake, const Plane &p_camera_plane, float p_lod_distance_multiplier, float p_screen_lod_threshold, const Rect2i &p_rect, bool p_flip_y, bool p_clear_region, bool p_begin, bool p_end, RendererScene::RenderInfo *p_render_info) {
 	uint32_t shadow_pass_index = scene_state.shadow_passes.size();
 
 	SceneState::ShadowPass shadow_pass;
