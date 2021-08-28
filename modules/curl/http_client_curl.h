a99195f143cfe05b18033ff9a45659dc82785c4c
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-08-28 16:31:12 -0700

    Update style.

--- modules/curl/http_client_curl.h
+++ modules/curl/http_client_curl.h
@@ -1,12 +1,12 @@
 /*************************************************************************/
-/*  http_client.h                                                        */
+/*  http_client_curl.h                                                   */
 /*************************************************************************/
 /*                       This file is part of:                           */
 /*                           GODOT ENGINE                                */
 /*                      https://godotengine.org                          */
 /*************************************************************************/
-/* Copyright (c) 2007-2020 Juan Linietsky, Ariel Manzur.                 */
-/* Copyright (c) 2014-2020 Godot Engine contributors (cf. AUTHORS.md).   */
+/* Copyright (c) 2007-2021 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2021 Godot Engine contributors (cf. AUTHORS.md).   */
 /*                                                                       */
 /* Permission is hereby granted, free of charge, to any person obtaining */
 /* a copy of this software and associated documentation files (the       */
@@ -52,7 +52,7 @@ struct RequestContext {
 };
 
 class HTTPClientCurl : public HTTPClient {
-	static const char * methods[10];
+	static const char *methods[10];
 	static size_t _header_callback(char *buffer, size_t size, size_t nitems, void *userdata);
 	static size_t _read_callback(char *buffer, size_t size, size_t nitems, void *userdata);
 	static size_t _write_callback(char *buffer, size_t size, size_t nitems, void *userdata);
