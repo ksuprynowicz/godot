7790215bf6f9aa2b73697aedef36da6c36ae994a
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-09-11 22:33:41 -0700

    Add packaging info.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Godot.Mixfile do
       compilers: [:elixir_make, :unifex, :bundlex] ++ Mix.compilers,
       version: "0.1.0",
       deps: deps(),
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      package: package(),
    ]
   end
 
@@ -21,6 +22,16 @@ defmodule Godot.Mixfile do
     [
       {:unifex, "~> 0.7.0"},
       {:elixir_make, "~> 0.4", runtime: false},
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      files: [
+        "lib", "LICENSE.txt", "mix.exs", "README.md",
+        "bin", "c_src", "c_src/*.md", "Makefile"],
+      links: %{"GitHub" => "https://github.com/V-Sekai/godot/tree/elixir"}
     ]
   end
 end
