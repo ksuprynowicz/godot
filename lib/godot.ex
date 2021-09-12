416ebdd4488d3e883e9cab84388d30b235ed05e3
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-09-12 00:13:17 -0700

    Remove function call.

--- lib/godot.ex
+++ lib/godot.ex
@@ -9,7 +9,6 @@ defmodule Godot do
       {:ok, pid} = Unifex.CNode.start_link(:godot_elixir)
       state = %GodotState{pid: pid, last_tick: :os.system_time(:millisecond)}
       Unifex.CNode.call(state.pid, :init, [["godot"] ++ args])
-      schedule_work() # Schedule work to be performed on start
       {:ok, state}
   end
 end
