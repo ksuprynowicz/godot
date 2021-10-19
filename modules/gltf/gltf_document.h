21c3650204e05dd3b0b74af7bca53a267bd67d95
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-10-19 09:06:23 -0700

    Add import glb from buffer
    Split functions from gltf document import and export into six functions.
    
    Use base path to allow two code paths based on an empty base path or a full base path.
    
    Add uri decode in _parse_buffers.
    
    Scan the filesystem when saving.
    523e0d

--- modules/gltf/gltf_document.h
+++ modules/gltf/gltf_document.h
@@ -33,6 +33,7 @@
 
 #include "gltf_animation.h"
 
+#include "core/error/error_list.h"
 #include "core/variant/dictionary.h"
 #include "core/variant/variant.h"
 #include "gltf_document_extension_convert_importer_mesh.h"
@@ -46,8 +47,9 @@
 #include "scene/resources/material.h"
 #include "scene/resources/texture.h"
 
-#include "modules/modules_enabled.gen.h"
 #include <cstdint>
+
+#include "modules/modules_enabled.gen.h"
 
 class GLTFState;
 class GLTFSkin;
@@ -119,11 +121,6 @@ protected:
 	static void _bind_methods();
 
 public:
-	Node *import_scene(const String &p_path, uint32_t p_flags, int32_t p_bake_fps, Ref<GLTFState> r_state);
-	Node *import_scene_gltf(const String &p_path, uint32_t p_flags, int32_t p_bake_fps, Ref<GLTFState> r_state, List<String> *r_missing_deps, Error *r_err = nullptr);
-	Error save_scene(Node *p_node, const String &p_path,
-			const String &p_src_path, uint32_t p_flags,
-			float p_bake_fps, Ref<GLTFState> r_state);
 	void set_extensions(TypedArray<GLTFDocumentExtension> p_extensions);
 	TypedArray<GLTFDocumentExtension> get_extensions() const;
 
@@ -199,7 +196,7 @@ private:
 	Ref<Texture2D> _get_texture(Ref<GLTFState> state,
 			const GLTFTextureIndex p_texture);
 	Error _parse_json(const String &p_path, Ref<GLTFState> state);
-	Error _parse_glb(const String &p_path, Ref<GLTFState> state);
+	Error _parse_glb(FileAccess *f, Ref<GLTFState> state);
 	void _compute_node_heights(Ref<GLTFState> state);
 	Error _parse_buffers(Ref<GLTFState> state, const String &p_base_path);
 	Error _parse_buffer_views(Ref<GLTFState> state);
@@ -286,10 +283,10 @@ private:
 			Skeleton3D *skeleton,
 			const GLTFNodeIndex node_index,
 			const GLTFNodeIndex bone_index);
-	ImporterMeshInstance3D *_generate_mesh_instance(Ref<GLTFState> state, Node *parent_node, const GLTFNodeIndex node_index);
-	Camera3D *_generate_camera(Ref<GLTFState> state, Node *parent_node, const GLTFNodeIndex node_index);
-	Node3D *_generate_light(Ref<GLTFState> state, Node *parent_node, const GLTFNodeIndex node_index);
-	Node3D *_generate_spatial(Ref<GLTFState> state, Node *parent_node, const GLTFNodeIndex node_index);
+	ImporterMeshInstance3D *_generate_mesh_instance(Ref<GLTFState> state, const GLTFNodeIndex node_index);
+	Camera3D *_generate_camera(Ref<GLTFState> state, const GLTFNodeIndex node_index);
+	Node3D *_generate_light(Ref<GLTFState> state, const GLTFNodeIndex node_index);
+	Node3D *_generate_spatial(Ref<GLTFState> state, const GLTFNodeIndex node_index);
 	void _assign_scene_names(Ref<GLTFState> state);
 	template <class T>
 	T _interpolate_track(const Vector<float> &p_times, const Vector<T> &p_values,
@@ -360,6 +357,7 @@ private:
 			GLTFNodeIndex p_node_i);
 	Error _encode_buffer_bins(Ref<GLTFState> state, const String &p_path);
 	Error _encode_buffer_glb(Ref<GLTFState> state, const String &p_path);
+	PackedByteArray _serialize_glb_buffer(Ref<GLTFState> state, Error *r_err);
 	Dictionary _serialize_texture_transform_uv1(Ref<BaseMaterial3D> p_material);
 	Dictionary _serialize_texture_transform_uv2(Ref<BaseMaterial3D> p_material);
 	Error _serialize_version(Ref<GLTFState> state);
@@ -383,6 +381,17 @@ private:
 	static float get_max_component(const Color &p_color);
 
 public:
+	Error load_from_file(String p_path, uint32_t p_flags, int32_t p_bake_fps, Ref<GLTFState> r_state);
+	Error load_from_buffer(Array p_bytes, String p_base_path, uint32_t p_flags, int32_t p_bake_fps, Ref<GLTFState> r_state);
+	Error load_from_scene(Node *p_node, uint32_t p_flags, int32_t p_bake_fps, Ref<GLTFState> r_state);
+
+public:
+	Node *generate_scene(float p_bake_fps, Ref<GLTFState> state);
+	Error write_to_file(const String &p_path, Ref<GLTFState> state);
+	PackedByteArray write_to_buffer(Ref<GLTFState> state);
+
+public:
+	Error _parse_gltf_state(Ref<GLTFState> state, const String &p_search_path, float p_bake_fps);
 	void _process_mesh_instances(Ref<GLTFState> state, Node *scene_root);
 	void _generate_scene_node(Ref<GLTFState> state, Node *scene_parent,
 			Node3D *scene_root,
@@ -446,8 +455,8 @@ public:
 			MeshInstance3D *p_mesh_instance);
 	void _convert_animation(Ref<GLTFState> state, AnimationPlayer *ap,
 			String p_animation_track_name);
-	Error serialize(Ref<GLTFState> state, Node *p_root, const String &p_path);
-	Error parse(Ref<GLTFState> state, String p_paths, bool p_read_binary = false);
+	Error _serialize(Ref<GLTFState> state, const String &p_path);
+	Error _parse(Ref<GLTFState> state, String p_path, FileAccess *f, int p_bake_fps);
 };
 
 #endif // GLTF_DOCUMENT_H
