a52f4c6abd1d36ffa87611d55c8485f99c5b2ab9
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-11-11 13:35:30 -0800

    Add support for importing and exporting .blend files
    Lets you drag or place .blend files in the project folder and it will import the files.
    
    An editor setting sets the location of the blender binary. A empty setting means the feature is disabled.
    
    Checks for Blender 3.0's gltf2 keep texture option.

---
+++ modules/gltf_blend/config.py
@@ -0,0 +1,17 @@
+def can_build(env, platform):
+    return not env["disable_3d"]
+
+
+def configure(env):
+    pass
+
+
+def get_doc_classes():
+    return [
+        "EditorSceneFormatImporterBlend",
+        "SceneExporterBlendPlugin",
+    ]
+
+
+def get_doc_path():
+    return "doc_classes"
