ebc040d59fb18a2f28f1da950a12ceaaec5f4989
Lyuma <xn.lyuma@gmail.com>
2021-11-15 01:22:38 -0800

    Change Node3D Gizmo to call group in realtime.
    Averts godot crash which occurs if a node is added and then removed from the edited scene within one frame.
    
    # Conflicts:
    #	scene/3d/node_3d.cpp

--- scene/3d/node_3d.cpp
+++ scene/3d/node_3d.cpp
@@ -180,7 +180,7 @@ void Node3D::_notification(int p_what) {
 
 #ifdef TOOLS_ENABLED
 			if (Engine::get_singleton()->is_editor_hint() && get_tree()->is_node_being_edited(this)) {
-				get_tree()->call_group_flags(0, SceneStringNames::get_singleton()->_spatial_editor_group, SceneStringNames::get_singleton()->_request_gizmo, this);
+				get_tree()->call_group_flags(SceneTree::GROUP_CALL_REALTIME, SceneStringNames::get_singleton()->_spatial_editor_group, SceneStringNames::get_singleton()->_request_gizmo, this);
 			}
 #endif
 		} break;
