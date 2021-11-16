b0909ee0e3e158f994dd359895b330fc094bd60d
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-11-16 00:09:16 -0800

    Add support for importing .blend files
    Lets you drag or place .blend files in the project folder and it will import the files.
    
    An editor setting sets the location of the blender binary. 
    
    Checks for Blender 3.0's gltf2 `export_keep_originals` option.

--- modules/gltf_blend/register_types.h
+++ modules/gltf_blend/register_types.h
@@ -1,5 +1,5 @@
 /*************************************************************************/
-/*  resource_importer_obj.h                                              */
+/*  register_types.h                                                     */
 /*************************************************************************/
 /*                       This file is part of:                           */
 /*                           GODOT ENGINE                                */
@@ -28,46 +28,5 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 
-#ifndef RESOURCEIMPORTEROBJ_H
-#define RESOURCEIMPORTEROBJ_H
-
-#include "resource_importer_scene.h"
-
-class EditorOBJImporter : public EditorSceneFormatImporter {
-	GDCLASS(EditorOBJImporter, EditorSceneFormatImporter);
-
-public:
-	virtual uint32_t get_import_flags() const override;
-	virtual void get_extensions(List<String> *r_extensions) const override;
-	virtual Node *import_scene(const String &p_path, uint32_t p_flags, const Dictionary &p_options, int p_bake_fps, List<String> *r_missing_deps, Error *r_err = nullptr) override;
-	virtual Ref<Animation> import_animation(const String &p_path, uint32_t p_flags, const Dictionary &p_options, int p_bake_fps) override;
-
-	EditorOBJImporter();
-};
-
-class ResourceImporterOBJ : public ResourceImporter {
-	GDCLASS(ResourceImporterOBJ, ResourceImporter);
-
-public:
-	virtual String get_importer_name() const override;
-	virtual String get_visible_name() const override;
-	virtual void get_recognized_extensions(List<String> *p_extensions) const override;
-	virtual String get_save_extension() const override;
-	virtual String get_resource_type() const override;
-	virtual int get_format_version() const override;
-
-	virtual int get_preset_count() const override;
-	virtual String get_preset_name(int p_idx) const override;
-
-	virtual void get_import_options(const String &p_path, List<ImportOption> *r_options, int p_preset = 0) const override;
-	virtual bool get_option_visibility(const String &p_path, const String &p_option, const Map<StringName, Variant> &p_options) const override;
-
-	virtual Error import(const String &p_source_file, const String &p_save_path, const Map<StringName, Variant> &p_options, List<String> *r_platform_variants, List<String> *r_gen_files = nullptr, Variant *r_metadata = nullptr) override;
-
-	// Threaded import can currently cause deadlocks, see GH-48265.
-	virtual bool can_import_threaded() const override { return false; }
-
-	ResourceImporterOBJ();
-};
-
-#endif // RESOURCEIMPORTEROBJ_H
+void register_gltf_blend_types();
+void unregister_gltf_blend_types();
