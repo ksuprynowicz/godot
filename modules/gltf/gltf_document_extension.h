db3ffad4bd2d26d950a85e3e2c181b59b936afe3
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-02-04 06:12:40 -0800

    Apply feedback from gltf extension usage to resolve flaws in the GLTFDocument api.

--- modules/gltf/gltf_document_extension.h
+++ modules/gltf/gltf_document_extension.h
@@ -35,7 +35,13 @@
 #include "core/variant/dictionary.h"
 #include "core/variant/typed_array.h"
 #include "core/variant/variant.h"
-class GLTFDocument;
+
+#include "core/object/gdvirtual.gen.inc"
+#include "core/object/script_language.h"
+#include "core/variant/native_ptr.h"
+
+class GLTFState;
+class GLTFNode;
 class GLTFDocumentExtension : public Resource {
 	GDCLASS(GLTFDocumentExtension, Resource);
 
@@ -46,18 +52,29 @@ protected:
 	static void _bind_methods();
 
 public:
+	virtual bool is_changing_original_tree() const;
 	virtual Array get_import_setting_keys() const;
-	virtual Variant get_import_setting(const StringName &p_key) const;
-	virtual void set_import_setting(const StringName &p_key, Variant p_var);
-	virtual Error import_preflight(Ref<GLTFDocument> p_document) { return OK; }
-	virtual Error import_post(Ref<GLTFDocument> p_document, Node *p_node) { return OK; }
-
-public:
+	virtual Variant get_import_setting(const StringName p_key) const;
+	virtual void set_import_setting(const StringName p_key, Variant p_var);
 	virtual Array get_export_setting_keys() const;
-	virtual Variant get_export_setting(const StringName &p_key) const;
-	virtual void set_export_setting(const StringName &p_key, Variant p_var);
-	virtual Error export_preflight(Ref<GLTFDocument> p_document, Node *p_node) { return OK; }
-	virtual Error export_post(Ref<GLTFDocument> p_document) { return OK; }
+	virtual Variant get_export_setting(const StringName p_key) const;
+	virtual void set_export_setting(const StringName p_key, Variant p_var);
+	virtual Error import_preflight(Ref<GLTFState> p_state);
+	virtual Error import_post_parse(Ref<GLTFState> p_state);
+	virtual Error export_post(Ref<GLTFState> p_state);
+	// These stages should not be able to modify the original node when checking conditions.
+	virtual Error import_post(Ref<GLTFState> p_state, Node *p_node);
+	virtual Error export_preflight(Node *p_state);
+	// End condition checking stages.
+	Error import_node(Ref<GLTFState> p_state, Ref<GLTFNode> p_gltf_node, Dictionary &r_dict, Node *p_node);
+	Error export_node(Ref<GLTFState> p_state, Ref<GLTFNode> p_gltf_node, Dictionary &r_dict, Node *p_node);
+	GDVIRTUAL1R(int, _import_preflight, RES);
+	GDVIRTUAL1R(int, _import_post_parse, RES);
+	GDVIRTUAL4R(int, _import_node, RES, RES, Dictionary, Object *);
+	GDVIRTUAL2R(int, _import_post, RES, Object *);
+	GDVIRTUAL1R(int, _export_preflight, Object *);
+	GDVIRTUAL4R(int, _export_node, RES, RES, Dictionary, Object *);
+	GDVIRTUAL1R(int, _export_post, RES);
 };
 
 #endif // GLTF_DOCUMENT_EXTENSION_H
