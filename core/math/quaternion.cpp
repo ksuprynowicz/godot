9990e95325ca2affc390097af436f32ce3d43fd5
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-02-21 20:23:54 -0800

    Normalize the quaternion.

--- core/math/quaternion.cpp
+++ core/math/quaternion.cpp
@@ -223,7 +223,7 @@ Quaternion Quaternion::cubic_slerp(const Quaternion &p_b, const Quaternion &p_pr
 		ln.get_axis_angle(axis, angle);
 		Vector3 src_v = axis * angle;
 		float theta = src_v.length();
-		ret = Quaternion(src_v.normalized(), theta);
+		ret = Quaternion(src_v.normalized(), theta).normalized();
 	} else {
 		ret.x = Math::cubic_interpolate(ret.x, q_b.x, prep.x, post_b.x, p_weight);
 		ret.y = Math::cubic_interpolate(ret.y, q_b.y, prep.y, post_b.y, p_weight);
